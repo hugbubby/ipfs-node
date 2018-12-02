427102f694664d4206dbb7a6c2486948ea29a7aa
hugbubby <hugbubby@protonmail.com>
2018-12-02 11:10:59 -0600

    Large update and change to design.

--- src/config.go
+++ src/config.go
@@ -1,44 +1,22 @@
 package main
 
 import "encoding/json"
-import "strings"
 import "os"
-import "net/url"
 
 //Main configuration structure.
 //Unmarshal json config into one of these
 //structs.
 type configuration struct {
-	ServerURL string `json:"serverURL"`
-	IpfsURL   string `json:"ipfsURL"`
+	ServerAddress string `json:"serverAddress"`
+	IpfsAddress   string `json:"ipfsAddress"`
 }
 
 func (config *configuration) loadFile(filename string) error {
 	var err error
 	file, err := os.Open(filename)
 	if err == nil {
-		var filedirname string
-		if filename != "" {
-			i := strings.LastIndex(filename, "\\")
-			if i == -1 {
-				i = strings.LastIndex(filename, "/")
-				if i == -1 {
-					i = 0
-				}
-			}
-			filedirname = filename[0:i]
-		} else {
-			filedirname = ""
-		}
-		filedir, err := os.Open(filedirname)
-		if err == nil {
-			err = filedir.Chdir()
-			if err == nil {
-				decoder := json.NewDecoder(file)
-				err = decoder.Decode(config)
-			}
-		}
-		filedir.Close()
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(config)
 	}
 	file.Close()
 	return err
@@ -47,15 +25,8 @@ func (config *configuration) loadFile(filename string) error {
 func (config *configuration) makeServer() server {
 	var s server
 
-	ipfsURL, err := url.Parse(config.IpfsURL)
-	if err == nil {
-		s.ipfsURL = ipfsURL
-	}
-
-	serverURL, err := url.Parse(config.ServerURL)
-	if err == nil {
-		s.url = serverURL
-	}
+	s.ipfsAddress = config.IpfsAddress
+	s.address = config.ServerAddress
 
 	return s
 }
---
+++ src/server.go
@@ -0,0 +1,138 @@
+package main
+
+import "fmt"
+import "log"
+import "io"
+import "io/ioutil"
+import "net/http"
+import "crypto/x509"
+import "encoding/pem"
+import "github.com/dgrijalva/jwt-go"
+
+type server struct {
+	address      string //URL of server.
+	ipfsAddress  string
+	tlsCertPath  string
+	tlsKeyPath   string
+	tokenKey     interface{} //RSA, ECDSA, ETC
+	tokenKeyPath string
+}
+
+func (s *server) getTokenKeyPath() string {
+	if s.tokenKeyPath == "" {
+		s.tokenKeyPath = "security/tokens/privkey.pem"
+	}
+	return s.tokenKeyPath
+}
+
+func (s *server) getTLSKeyPath() string {
+	if s.tlsKeyPath == "" {
+		s.tlsKeyPath = "security/tls/privkey.pem"
+	}
+	return s.tlsKeyPath
+}
+
+func (s *server) getTLSCertPath() string {
+	if s.tlsCertPath == "" {
+		s.tlsCertPath = "security/tls/host.cert"
+	}
+	return s.tlsCertPath
+}
+
+func (s *server) getIPFSAddress() string {
+	if s.ipfsAddress == "" {
+		s.ipfsAddress = "http://127.0.0.1:5001"
+	}
+	return s.ipfsAddress
+}
+
+func (s *server) getAddress() string {
+	if s.address == "" {
+		s.address = "http://127.0.0.1:25566"
+	}
+	return s.address
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func (s *server) validToken(token *jwt.Token) bool {
+	err := token.Claims.Valid()
+	isValid := err != nil
+	if isValid {
+		log.Println(err)
+	}
+	return isValid
+}
+
+func (s *server) getTokenKey(token *jwt.Token) (interface{}, error) {
+	var err error
+	if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+		err = fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+	} else {
+		if s.tokenKey == nil {
+			var bytes []byte
+			bytes, err = ioutil.ReadFile(s.getTokenKeyPath())
+			if err == nil {
+				block, _ := pem.Decode(bytes)
+				if block != nil {
+					var key interface{}
+					key, err = x509.ParsePKCS8PrivateKey(block.Bytes)
+					if err == nil {
+						s.tokenKey = key
+					}
+				} else {
+					err = fmt.Errorf("failed to decode token authentication key - is it in the PEM format?")
+				}
+			}
+		}
+	}
+	return s.tokenKey, err
+}
+
+func (s *server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" { //IPFS HTTP API only supports get requests.
+		tokenS := req.FormValue("ipfsrToken")
+		if tokenS != "" {
+			t, err := jwt.Parse(tokenS, s.getTokenKey)
+			if err == nil {
+				if s.validToken(t) {
+					var resp *http.Response
+					var err error
+					if req.FormValue("arg") == "" {
+						resp, err = http.Get(s.getIPFSAddress() + req.URL.Path)
+					} else {
+						resp, err = http.Get(s.getIPFSAddress() + req.URL.Path + "?arg=" + req.FormValue("arg"))
+					}
+					if err == nil {
+						copyHeader(rw.Header(), resp.Header)
+						rw.WriteHeader(resp.StatusCode)
+						io.Copy(rw, resp.Body)
+						resp.Body.Close()
+					} else {
+						log.Printf("failed to forward authenticated request to IPFS server, %v", err)
+					}
+				}
+			} else {
+				log.Printf("error while attempting to parse token from string: %v", err)
+			}
+		}
+	}
+}
+
+func (s *server) start() {
+	address := s.getAddress()
+	certificatePath := s.getTLSCertPath()
+	keyPath := s.getTLSKeyPath()
+
+	log.Println(address)
+	err := http.ListenAndServeTLS(address, certificatePath, keyPath, s)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
